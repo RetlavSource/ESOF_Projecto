fc897e558c94170377a30f64d4857be281a4f9e1
RetlavSource <valterinfomundo@gmail.com>
2019-11-25 09:16:12 +0000

    Update AtendimentoRepo.java

--- src/main/java/com/projeto/gestao_explicacoes/repositories/AtendimentoRepo.java
+++ src/main/java/com/projeto/gestao_explicacoes/repositories/AtendimentoRepo.java
@@ -1,4 +1,17 @@
 package com.projeto.gestao_explicacoes.repositories;
 
-public interface AtendimentoRepo {
+import com.projeto.gestao_explicacoes.models.Aluno;
+import com.projeto.gestao_explicacoes.models.Atendimento;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AtendimentoRepo extends CrudRepository<Atendimento, Long> {
+
+    Optional<Atendimento> findByAluno(Aluno aluno);
+    Optional<Atendimento> findByAluno_Nome(String nome);
+    Optional<Atendimento> findByAluno_Numero(Integer numero);
+
 }
