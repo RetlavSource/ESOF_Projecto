a3b4b2b2b8753034ea4b31b3b35cc87f4acd3706
RetlavSource <valterinfomundo@gmail.com>
2019-12-18 18:48:11 +0000

    reorganizadas as excepções

--- src/main/java/com/projeto/gestao_explicacoes/controllers/AlunoController.java
+++ src/main/java/com/projeto/gestao_explicacoes/controllers/AlunoController.java
@@ -1,11 +1,12 @@
 package com.projeto.gestao_explicacoes.controllers;
 
+import com.projeto.gestao_explicacoes.exceptions.FalhaCriarException;
+import com.projeto.gestao_explicacoes.exceptions.FalhaPesquisaException;
 import com.projeto.gestao_explicacoes.models.Aluno;
 import com.projeto.gestao_explicacoes.services.AlunoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -44,7 +45,7 @@ public class AlunoController {
             return ResponseEntity.ok(optionalAluno.get());
         }
         //return ResponseEntity.notFound().build();
-        throw new NoAlunoException(id);
+        throw new FalhaPesquisaException("Aluno com o id: " + id + " inexistente!");
     }
 
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -54,36 +55,11 @@ public class AlunoController {
         Optional<Aluno> criadoAluno = this.alunoService.criarAluno(aluno);
         
         if (criadoAluno.isEmpty()){
-            throw new AlunoAlreadyExistsException(aluno.getNome());
+            throw new FalhaCriarException("O aluno com o nome: " + aluno.getNome() + " ja existe!");
         }
         return ResponseEntity.ok(criadoAluno.get());
     }
 
-
-
-
-
-    // *******************************************
-    // ****** RESPONSE STATUS -- EXCEPTIONS ******
-    // *******************************************
-
-    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Aluno inexistente!")
-    private class NoAlunoException extends RuntimeException{
-
-        public NoAlunoException(Long id){
-            super("Aluno inexistente com o id: " + id);
-        }
-
-    }
-
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Aluno ja existe!")
-    private class AlunoAlreadyExistsException extends RuntimeException{
-
-        public AlunoAlreadyExistsException(String nome){
-            super("O aluno com o nome: " + nome + " ja existe!");
-        }
-
-    }
 
 }
 
